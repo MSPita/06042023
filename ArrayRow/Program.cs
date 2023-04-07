cf8f7fbec33bb6ae848a493e5d48153d222b44ae
Maria Pita <mahoni@bk.ru>
2023-04-07 19:58:13 +0300

    2

--- ArrayRow/Program.cs
+++ ArrayRow/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количесво строк: ");
+int userArraySize = int.Parse(Console.ReadLine()!);
+
+// Инициализирую пользовательский массив строк нужного размера
+string[] Array(int size)
+{
+    string[] array = new string[size];
+    return array;
+}
+
+// Заполняю массив значениями введенными пользователем
+void FillArray(string[] array)
+{
+    int j = 0;
+    for (int i = 1; i <= userArraySize; i++)
+    {
+        Console.WriteLine($"Введите {i} строку");
+        array[j] = Console.ReadLine()!;
+        j++;
+    }
+}
+
+// Вывожу массив, который ввел пользователь в консоль
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        else if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else
+        {
+            Console.Write($"{array[i]}]");
+            Console.WriteLine();
+        }
+
+    }
+    if (array.Length == 0) Console.WriteLine("[]");
+}
+
+// Считаю количество элементов для заполнения результирующего массива
+int GetResultArraySize(string[] array)
+{
+    int resultArraySize = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) resultArraySize++;
+    }
+    return resultArraySize;
+}
+
+// Заполняю массив валидными элементами
+void FillResultArray(string[] userArray, string[] resultArray)
+{
+    int resultArrElem = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+        {
+            resultArray[resultArrElem] = userArray[i];
+            resultArrElem++;
+        }
+
+    }
+}
+
+string[] userArray = Array(userArraySize);
+FillArray(userArray);
+Console.WriteLine();
+PrintArray(userArray);
+int resArraySize = GetResultArraySize(userArray);
+string[] resArray = Array(resArraySize);
+FillResultArray(userArray, resArray);
+Console.WriteLine();
+PrintArray(resArray);
+
+
